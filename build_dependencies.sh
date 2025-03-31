d273829178d039b4642c60e7a16cbf614d7af7b6
KotDath <kotdath@yandex.ru>
2025-03-31 23:55:30 +0300

    [Project] Add all dependencies.

--- build_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd $1/_deps/openblas-src
-/usr/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DDYNAMIC_ARCH=ON -DTARGET=$2 -DBINARY=$3 .
-make -j$(nproc)
-popd
--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,69 @@
 
 #include <auroraapp.h>
 #include <QtQuick>
+#include <mlpack.hpp>
 
-#include <cblas.h>
-#include <stdio.h>
-
+using namespace mlpack;
+using namespace ens;
+using namespace mlpack::rl;
 int main(int argc, char *argv[])
 {
+    // Set up the network.
+    FFN<MeanSquaredError, GaussianInitialization> network(MeanSquaredError(),
+    GaussianInitialization(0, 0.001));
+    network.Add<Linear>(128);
+    network.Add<ReLU>();
+    network.Add<Linear>(128);
+    network.Add<ReLU>();
+    network.Add<Linear>(2);
+
+    SimpleDQN<> model(network);
+
+    // Set up the policy and replay method.
+    GreedyPolicy<CartPole> policy(1.0, 1000, 0.1, 0.99);
+    RandomReplay<CartPole> replayMethod(10, 10000);
+
+    TrainingConfig config;
+    config.StepSize() = 0.01;
+    config.Discount() = 0.9;
+    config.TargetNetworkSyncInterval() = 100;
+    config.ExplorationSteps() = 100;
+    config.DoubleQLearning() = false;
+    config.StepLimit() = 200;
+
+    QLearning<CartPole, decltype(model), AdamUpdate, decltype(policy)>
+          agent(config, model, policy, replayMethod);
+
+    arma::running_stat<double> averageReturn;
+    size_t episodes = 0;
+    bool converged = true;
+    while (true)
+    {
+      double episodeReturn = agent.Episode();
+      averageReturn(episodeReturn);
+      episodes += 1;
+
+      if (episodes > 1000)
+      {
+        std::cout << "Cart Pole with DQN failed." << std::endl;
+        converged = false;
+        break;
+      }
+
+      /**
+       * Reaching running average return 35 is enough to show it works.
+       */
+      std::cout << "Average return: " << averageReturn.mean()
+          << " Episode return: " << episodeReturn << std::endl;
+      if (averageReturn.mean() > 35)
+        break;
+    }
+    if (converged)
+      std::cout << "Hooray! Q-Learning agent successfully trained" << std::endl;
+
     QScopedPointer<QGuiApplication> application(Aurora::Application::application(argc, argv));
     application->setOrganizationName(QStringLiteral("ru.kotdath"));
     application->setApplicationName(QStringLiteral("MLPackAurora"));
-
-    int i=0;
-      double A[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};
-      double B[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};
-      double C[9] = {.5,.5,.5,.5,.5,.5,.5,.5,.5};
-      cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans,3,3,2,1,A, 3, B, 3,2,C,3);
-
-      for(i=0; i<9; i++)
-        printf("%lf ", C[i]);
-      printf("\n");
 
     QScopedPointer<QQuickView> view(Aurora::Application::createView());
     view->setSource(Aurora::Application::pathTo(QStringLiteral("qml/MLPackAurora.qml")));
